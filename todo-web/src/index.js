db337dffe0ca203d676b937450aa2232793b8d62
szulima <34941973+szulima@users.noreply.github.com>
2021-05-23 00:01:41 +0200

    add theme file and ThemeProvider componnent

--- todo-web/src/index.js
+++ todo-web/src/index.js
@@ -2,16 +2,20 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter as Router } from "react-router-dom";
 import { RecoilRoot } from "recoil";
+import { ThemeProvider } from "theme-ui";
+import { theme } from "./theme";
 import App from "./components/App";
 import "./index.css";
 
 ReactDOM.render(
   <React.StrictMode>
-    <RecoilRoot>
-      <Router>
-        <App />
-      </Router>
-    </RecoilRoot>
+    <ThemeProvider theme={theme}>
+      <RecoilRoot>
+        <Router>
+          <App />
+        </Router>
+      </RecoilRoot>
+    </ThemeProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );
---
+++ todo-web/src/theme.js
@@ -0,0 +1,8 @@
+export const theme = {
+  colors: {
+    text: "#fff",
+    background: "#1db09f",
+    primary: "#003e44",
+    accent: "#ffff00",
+  },
+};
