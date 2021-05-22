6cb50513bd76a7e03dab3ae7eb91c1ebcf5bdc57
szulima <34941973+szulima@users.noreply.github.com>
2021-05-22 12:56:17 +0200

    install theme-ui, add favicon, send fetch request to gorest.co.in API

--- todo-web/src/components/App.js
+++ todo-web/src/components/App.js
@@ -1,8 +1,18 @@
+import { useEffect } from "react";
 import { Switch, Route } from "react-router-dom";
 import HomePage from "../pages/HomePage";
 import ItemPage from "../pages/ItemPage";
 
 export default function App() {
+  // // get tasks
+  // useEffect(() => {
+  //   const endpoint = "https://gorest.co.in/public-api/users/1940/todos";
+  //   fetch(endpoint)
+  //     .then((response) => response.json())
+  //     .then((data) => console.log(data))
+  //     .catch((err) => console.log(err));
+  // }, []);
+
   return (
     <>
       <Switch>
